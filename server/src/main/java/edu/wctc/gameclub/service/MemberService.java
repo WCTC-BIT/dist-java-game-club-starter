576bb6cea5911a2434cccd8cac7083d3b34c5af1
Stacy Read <sread@ivaalsolutions.com>
2023-03-14 21:26:19 -0500

    getMember should throw exception if email not found

--- server/src/main/java/edu/wctc/gameclub/service/MemberService.java
+++ server/src/main/java/edu/wctc/gameclub/service/MemberService.java
@@ -6,13 +6,24 @@ import edu.wctc.gameclub.repo.MemberRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
 @Service
 public class MemberService {
     @Autowired
     private MemberRepository memberRepository;
 
-    public Member getMember(String email) {
-        return memberRepository.findByEmail(email);
+    public List<Member> getAllMembers() {
+        List<Member> list = new ArrayList<>();
+        memberRepository.findAll().forEach(list::add);
+        return list;
+    }
+
+    public Member getMember(String email) throws ResourceNotFoundException {
+        Optional<Member> optional = memberRepository.findByEmail(email);
+        return optional.orElseThrow(() -> new ResourceNotFoundException("Member", "email", email));
     }
 
     // New for v2
